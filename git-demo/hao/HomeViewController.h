16574bce2cfafd4645e10b00971b6c29109d5473
chgchceo <463219366@qq.com>
2024-05-18 23:09:29 +0800

    /hao/

---
+++ git-demo/hao/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  git-demo
+//
+//  Created by 程广成 on 2024/5/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ git-demo/hao/HomeViewController.m
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.m
+//  git-demo
+//
+//  Created by 程广成 on 2024/5/18.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    //haha
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
